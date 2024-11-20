9e3d3efd337b14edf364eb5ec55eef6ca562749b
mungyun <mungyun1234@naver.com>
2024-11-20 20:08:08 +0900

    Chore: 인덱스 페이지 초기화

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,3 @@
-import Dropdown from "@/components/dropdown/Dropdown";
-
-const ExampleComponent = () => {
-  const options = ["Option 1", "Option 2", "Option 3", "Option 4"];
-
-  return (
-    <div className="p-4">
-      <h2 className="mb-4 text-xl">드롭다운 사용 예시</h2>
-      <Dropdown label="가격" size="big" options={options} />
-      <Dropdown label="가격" size="large" options={options} />
-      <Dropdown label="가격" size="medium" options={options} />
-      <Dropdown label="가격" size="small" options={options} />
-    </div>
-  );
-};
-
-export default ExampleComponent;
+export default function Home() {
+  return <div>메인 페이지</div>;
+}
