d780d432662e3615b4f447983a5d63d5eac45dec
DrowsyzzZ <daenamu222@naver.com>
2024-11-21 14:01:23 +0900

    Design: img 태그를 Next.js Image 태그로 변경

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,9 @@
+import Image from "next/image";
+
 /* eslint-disable @next/next/no-img-element */
 const Footer = () => {
   return (
+    // eslint-disable-next-line prettier/prettier
     <div className="relative h-40 w-full bg-black02 px-[38px] pt-8 font-arial text-[#676767] md:px-[111px]">
       <div className="mx-auto flex max-w-[1200px] flex-wrap justify-center gap-3 overflow-hidden sm:justify-around lg:justify-between">
         <span className="whitespace-nowrap pb-3">©codeit - 2023</span>
@@ -8,11 +11,11 @@ const Footer = () => {
           <span className="whitespace-nowrap">Privacy Policy</span>
           <span className="pl-[30px] pr-[18.11px]">FAQ</span>
         </div>
-        <div className="flex w-fit gap-3">
-          <img src="/icons/facebook.svg" alt="페이스북 아이콘" className="h-5 w-5" />
-          <img src="/icons/twitter.svg" alt="트위터 아이콘" className="h-5 w-5" />
-          <img src="/icons/youtube.svg" alt="유튜브 아이콘" className="h-5 w-5" />
-          <img src="/icons/instagram.svg" alt="인스타그램 아이콘" className="h-5 w-5" />
+        <div className="flex h-5 w-fit gap-3">
+          <Image src="/icons/facebook.svg" alt="페이스북 아이콘" height={20} width={20} />
+          <Image src="/icons/twitter.svg" alt="트위터 아이콘" height={20} width={20} />
+          <Image src="/icons/youtube.svg" alt="유튜브 아이콘" height={20} width={20} />
+          <Image src="/icons/instagram.svg" alt="인스타그램 아이콘" height={20} width={20} />
         </div>
       </div>
     </div>
