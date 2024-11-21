d1c5ea2ea8caf39a5b3ff527388d7e0f39a3ba92
BugO <kco796@naver.com>
2024-11-21 14:54:33 +0900

    Chore: input label 수정

--- src/components/input/AuthInput.tsx
+++ src/components/input/AuthInput.tsx
@@ -15,8 +15,8 @@ const AuthInput = ({ label, errors, ...props }: InputProps) => {
 
   const errorClass = "border-red03";
   return (
-    <div className="flex flex-col gap-2">
-      {label && <label className="text-base leading-[26px] text-black03">{label}</label>}
+    <label className="flex flex-col gap-2 text-base leading-[26px] text-black03">
+      {label}
       <div className="relative">
         <input
           className={`h-[58px] w-full rounded-md border-[1px] border-gray08 px-5 py-4 text-base leading-[26px] outline-green02 placeholder:text-gray06 ${errors && errorClass}`}
@@ -37,7 +37,7 @@ const AuthInput = ({ label, errors, ...props }: InputProps) => {
         )}
       </div>
       {errors && <span className="pl-2 text-xs leading-[18px] text-red03">{errors}</span>}
-    </div>
+    </label>
   );
 };
 
