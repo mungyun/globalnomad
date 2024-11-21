f57685a2102fbfc7e218694dc44ede18709ee5c9
mungyun <mungyun1234@naver.com>
2024-11-21 16:15:52 +0900

    Chore: mockData 분리

--- src/app/activities/[activityId]/components/Banner.tsx
+++ src/app/activities/[activityId]/components/Banner.tsx
@@ -1,42 +1,6 @@
 import Image from "next/image";
 import Dropdown from "./Dropdown";
-
-const mockData = {
-  id: 7,
-  userId: 21,
-  title: "함께 배우면 즐거운 스트릿댄스",
-  description: "둠칫 둠칫 두둠칫",
-  category: "문화·예술",
-  price: 10000,
-  address: "서울특별시 강남구 테헤란로 427",
-  bannerImageUrl: "/images/banner1.png",
-  subImages: [
-    {
-      id: 1,
-      imageUrl: "/images/subimage1.png",
-    },
-    {
-      id: 2,
-      imageUrl: "/images/subimage2.png",
-    },
-    {
-      id: 3,
-      imageUrl: "/images/subimage3.png",
-    },
-    {
-      id: 4,
-      imageUrl: "/images/subimage4.png",
-    },
-  ],
-  schedules: [
-    { id: 1, date: "2023-12-01", startTime: "12:00", endTime: "13:00" },
-    { id: 2, date: "2023-12-05", startTime: "12:00", endTime: "13:00" },
-  ],
-  reviewCount: 5,
-  rating: 4.74,
-  createdAt: "2023-12-31T21:28:50.589Z",
-  updatedAt: "2023-12-31T21:28:50.589Z",
-};
+import { mockData } from "./mockdata";
 
 const Banner = () => {
   const { category, title, rating, reviewCount, address, bannerImageUrl, subImages } = mockData;
