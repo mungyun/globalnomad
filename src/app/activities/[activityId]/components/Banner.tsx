1f1ffdff3b432338445b9ee0d8241f9856205d8b
mungyun <mungyun1234@naver.com>
2024-11-21 16:12:14 +0900

    Feat: 체험 상세 페이지 Banner 반응형 구현

--- src/app/activities/[activityId]/components/Banner.tsx
+++ src/app/activities/[activityId]/components/Banner.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Dropdown from "./Dropdown";
 
 const mockData = {
   id: 7,
@@ -41,10 +42,13 @@ const Banner = () => {
   const { category, title, rating, reviewCount, address, bannerImageUrl, subImages } = mockData;
 
   return (
-    <div className="mb-[85px] mt-[78px]">
-      <div>
+    <div className="my-4 md:mb-8 md:mt-6 xl:mb-[85px] xl:mt-[78px]">
+      <div className="px-4 md:px-0">
         <span className="mb-[10px] text-[14px] text-black02">{category}</span>
-        <h2 className="mb-4 text-[32px] font-bold text-black02">{title}</h2>
+        <div className="mb-4 flex justify-between">
+          <h2 className="text-[24px] font-bold text-black02 md:text-[32px]">{title}</h2>
+          <Dropdown />
+        </div>
         <div className="mb-[25px] flex gap-3">
           <div className="flex gap-[6px]">
             <Image src="/icons/star.svg" alt="별" width={16} height={16} />
@@ -58,11 +62,24 @@ const Banner = () => {
           </div>
         </div>
       </div>
-      <div className="flex gap-2 rounded-xl">
-        <Image src={bannerImageUrl} alt="배너 이미지" width={595} height={534} />
-        <div className="flex flex-wrap gap-2">
+      <div className="flex justify-center gap-1 md:rounded-xl xl:gap-2">
+        <Image
+          src={bannerImageUrl}
+          alt="배너 이미지"
+          width={345}
+          height={310}
+          className="h-[310px] w-[375px] md:w-[345px] md:rounded-l-xl xl:h-[534px] xl:w-[595px]"
+        />
+        <div className="hidden rounded-r-xl md:flex md:flex-wrap md:gap-1 xl:gap-2">
           {subImages.map((item) => (
-            <Image src={item.imageUrl} alt="배너 이미지" width={294} height={263} key={item.id} />
+            <Image
+              src={item.imageUrl}
+              alt="배너 이미지"
+              width={294}
+              height={263}
+              key={item.id}
+              className="h-[153px] w-[170px] xl:h-[263px] xl:w-[294px]"
+            />
           ))}
         </div>
       </div>
