4851ed10187f81e1b95812d8477671539be8a8c8
ckx9798 <ckx12@Naver.com>
2024-11-22 13:24:15 +0900

    Feat: size로 css 수정

--- src/app/components/ImageBox.tsx
+++ src/app/components/ImageBox.tsx
@@ -8,9 +8,9 @@ interface SizeProps {
 
 export default function ImageBox({ size = "md", img, children }: SizeProps) {
   const sizeProps = {
-    sm: "w-[167px] h-[167px] md:w-[206px] md:h-[206px] xl:w-[180px] xl:h-[180px]",
-    md: "w-[168px] h-[168px] md:w-[221px] md:h-[221px] xl:w-[283px] xl:h-[283px]",
-    lg: "w-[186px] h-[186px] md:w-[384px] md:h-[384px] xl:w-[384px] xl:h-[384px]",
+    sm: "size-[167px] md:size-[206px] xl:size-[180px]",
+    md: "size-[168px] md:size-[221px] xl:size-[283px]",
+    lg: "size-[186px] md:size-[384px] xl:size-[384px]",
   };
 
   const baseClass = "flex items-center justify-center overflow-hidden rounded-3xl relative";
