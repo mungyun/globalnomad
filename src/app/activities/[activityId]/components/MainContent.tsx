7fbf5c487f299dfcb8087608235c35c57b4c8084
mungyun <mungyun1234@naver.com>
2024-11-22 22:35:25 +0900

    Chore: SidaBar를 page로 옮김

--- src/app/activities/[activityId]/components/MainContent.tsx
+++ src/app/activities/[activityId]/components/MainContent.tsx
@@ -1,25 +1,21 @@
 import CommentList from "./CommentList";
 import KakaoMap from "./KakaoMap";
-import SideBar from "./SideBar";
 import { mockData } from "./mockdata";
 
 const MainContent = () => {
   const { description, address } = mockData;
   return (
-    <div className="flex gap-6 pb-48">
-      <div>
-        <div className="border-t border-t-gray08 xl:w-[790px]">
-          <div className="border-b border-b-gray08 xl:pb-[34px] xl:pt-10">
-            <h3 className="mb-4 text-[20px] font-bold text-black02">체험 설명</h3>
-            <p className="text-[16px] text-gray08">{description}</p>
-          </div>
-          <div className="xl:py-10">
-            <KakaoMap address={address} />
-          </div>
-          <CommentList />
+    <div>
+      <div className="border-t border-t-gray08 xl:w-[790px]">
+        <div className="border-b border-b-gray08 xl:pb-[34px] xl:pt-10">
+          <h3 className="mb-4 text-[20px] font-bold text-black02">체험 설명</h3>
+          <p className="text-[16px] text-gray08">{description}</p>
         </div>
+        <div className="xl:py-10">
+          <KakaoMap address={address} />
+        </div>
+        <CommentList />
       </div>
-      <SideBar />
     </div>
   );
 };
--- src/app/activities/[activityId]/page.tsx
+++ src/app/activities/[activityId]/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Banner from "./components/Banner";
 import MainContent from "./components/MainContent";
+import SideBar from "./components/SideBar";
 
 const ActivityDetailPage = () => {
   return (
     <div>
       <Banner />
-      <MainContent />
+      <div className="flex gap-6 pb-48">
+        <MainContent />
+        <SideBar />
+      </div>
     </div>
   );
 };
