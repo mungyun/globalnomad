05127b8be7852903817080378460fc7c6a4cd64b
mungyun <mungyun1234@naver.com>
2024-11-23 14:15:34 +0900

    Feat: 캐러셀 기능 추가

--- src/app/activities/[activityId]/components/Banner.tsx
+++ src/app/activities/[activityId]/components/Banner.tsx
@@ -1,9 +1,20 @@
+"use client";
+
+import useDeviceType from "@/hooks/useDeviceType";
 import Image from "next/image";
+import Carousel from "./Carousel";
 import Dropdown from "./Dropdown";
 import { mockData } from "./mockdata";
 
 const Banner = () => {
   const { category, title, rating, reviewCount, address, bannerImageUrl, subImages } = mockData;
+  const deviceType = useDeviceType();
+
+  // 배너 이미지와 보조 이미지를 하나의 배열로 준비
+  const images = [
+    { src: bannerImageUrl, alt: "배너 이미지" },
+    ...subImages.map((item) => ({ src: item.imageUrl, alt: "보조 이미지" })),
+  ];
 
   return (
     <div className="my-4 md:mb-8 md:mt-6 xl:mb-[85px] xl:mt-[78px]">
@@ -26,27 +37,35 @@ const Banner = () => {
           </div>
         </div>
       </div>
-      <div className="flex w-full justify-center gap-1 md:rounded-xl xl:gap-2">
-        <Image
-          src={bannerImageUrl}
-          alt="배너 이미지"
-          width={375}
-          height={310}
-          className="h-full w-full object-cover md:max-h-[534px] md:max-w-[50%] md:rounded-l-xl"
-        />
-        <div className="hidden rounded-r-xl md:grid md:grid-cols-2 md:grid-rows-2 md:gap-1 xl:gap-2">
-          {subImages.map((item) => (
-            <Image
-              src={item.imageUrl}
-              alt="보조 이미지"
-              width={375}
-              height={310}
-              key={item.id}
-              className="h-full w-full object-cover md:max-h-[263px] md:max-w-[293px]"
-            />
-          ))}
+      {/* 모바일과 데스크톱 레이아웃 분리 */}
+      {deviceType === "mobile" ? (
+        // 캐러셀 렌더링
+        <Carousel images={images} />
+      ) : (
+        <div className="flex w-full justify-center gap-1 md:rounded-xl xl:gap-2">
+          {/* 배너 이미지 */}
+          <Image
+            src={bannerImageUrl}
+            alt="배너 이미지"
+            width={375}
+            height={310}
+            className="h-full w-full object-cover md:max-h-[534px] md:max-w-[50%] md:rounded-l-xl"
+          />
+          {/* 보조 이미지 그리드 */}
+          <div className="rounded-r-xl md:grid md:grid-cols-2 md:grid-rows-2 md:gap-1 xl:gap-2">
+            {subImages.map((item) => (
+              <Image
+                src={item.imageUrl}
+                alt="보조 이미지"
+                width={375}
+                height={310}
+                key={item.id}
+                className="h-full w-full object-cover md:max-h-[263px] md:max-w-[293px]"
+              />
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
---
+++ src/app/activities/[activityId]/components/Carousel.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import React, { useState } from "react";
+
+interface ImageType {
+  src: string;
+  alt: string;
+}
+
+interface CarouselProps {
+  images: ImageType[];
+}
+
+const Carousel: React.FC<CarouselProps> = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  return (
+    <div className="relative mx-auto w-full">
+      <div>
+        <img src={images[currentIndex].src} alt={images[currentIndex].alt} className="object-cover" />
+      </div>
+      <button className="absolute left-4 top-1/2 -translate-y-1/2 transform" onClick={handlePrev}>
+        <Image src="/icons/prevbutton.svg" alt="이전 버튼" width={24} height={47} />
+      </button>
+      <button className="absolute right-4 top-1/2 -translate-y-1/2 transform" onClick={handleNext}>
+        <Image src="/icons/nextbutton.svg" alt="다음 버튼" width={24} height={47} />
+      </button>
+      <div className="absolute bottom-4 left-1/2 flex -translate-x-1/2 transform space-x-2">
+        {images.map((_, index) => (
+          <span
+            key={index}
+            className={`h-3 w-3 cursor-pointer rounded-full bg-black opacity-50 ${
+              index === currentIndex ? "bg-white opacity-100" : ""
+            }`}
+            onClick={() => setCurrentIndex(index)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
