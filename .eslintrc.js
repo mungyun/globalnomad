c506d0c20240c6175437fdea9eb32a8e4dc24be8
mungyun <mungyun1234@naver.com>
2024-11-25 14:34:45 +0900

    Chore: ESLint 플러그인 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,7 @@
 module.exports = {
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier", // prettier와 ESLint의 충돌 방지
-  ],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:@next/next/recommended", "prettier"],
   plugins: ["prettier"],
   rules: {
-    "prettier/prettier": "error", // Prettier 규칙을 ESLint에서 에러로 처리
+    "prettier/prettier": "error",
   },
 };
