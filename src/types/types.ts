d8c495f6c97fdea3b632909cf265a2b601f504ac
BugO <kco796@naver.com>
2024-11-26 18:06:49 +0900

    Feat: 체험 등록 form 추가

--- src/types/types.ts
+++ src/types/types.ts
@@ -29,3 +29,24 @@ export interface MyReservations {
   reservations: Reservation[]; // 예약 배열
   totalCount: number; // 총 예약 수
 }
+
+// 스케줄 타입
+export interface Schedule {
+  date: string;
+  startTime: string;
+  endTime: string;
+}
+
+// 체험 등록 body 타입
+export interface PostActivities {
+  title: string;
+  category: string;
+  description: string;
+  price: number;
+  address: string;
+  schedules: Schedule[];
+  bannerImageUrl?: string;
+  subImageUrls?: File[];
+  bannerImageFile?: File;
+  subImageFiles?: File[];
+}
