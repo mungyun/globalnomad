72f3f94bdde857982a4575d60002fde6cc7195aa
BugO <kco796@naver.com>
2024-11-27 18:25:17 +0900

    Chore: my 페이지 레이아웃 수정 및 footer 추가

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,14 +1,18 @@
 import SideNavigation from "@/components/SideNavigation";
+import Footer from "@/components/layout/Footer";
 import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="flex justify-center gap-6 px-4 py-6 md:px-5 xl:py-[72px]">
-      <div className="hidden md:block">
-        <SideNavigation />
+    <>
+      <div className="flex justify-center gap-6 px-4 py-6 md:px-5 xl:py-[72px]">
+        <div className="hidden md:block">
+          <SideNavigation />
+        </div>
+        {children}
       </div>
-      {children}
-    </div>
+      <Footer />
+    </>
   );
 };
 
