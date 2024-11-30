105157ad797e16aa77d0401080c416a551236cc8
mungyun <mungyun1234@naver.com>
2024-11-30 14:57:52 +0900

    Chore: 로그인 에러 핸들링 추가

--- src/lib/api/Auth.ts
+++ src/lib/api/Auth.ts
@@ -1,11 +1,22 @@
+import { isAxiosError } from "axios";
 import { proxy } from "./axiosInstanceApi";
 
 export const postLogin = async ({ email, password }: { email: string; password: string }) => {
   try {
     const res = await proxy.post("/api/auth/login", { email, password });
     if (res.status >= 200 && res.status < 300) return res.data;
-  } catch (error) {
-    console.error("로그인 요청 중 문제가 발생했습니다.", error);
-    throw new Error("서버 오류가 발생했습니다. 나중에 다시 시도해주세요.");
+  } catch (error: unknown) {
+    if (isAxiosError(error)) {
+      if (error.response?.status === 400) {
+        throw new Error("이메일 형식으로 작성해주세요.");
+      }
+
+      if (error.response?.status === 404) {
+        throw new Error("존재하지 않는 유저입니다.");
+      }
+    }
+    // 기타 에러 처리
+    console.error("에러 메시지: ", error instanceof Error ? error.message : error);
+    return { message: "서버 오류가 발생했습니다.", status: 500 };
   }
 };
