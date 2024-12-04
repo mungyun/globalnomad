290c0e32561727625836239d4dd2a0c335a0c414
ckx9798 <ckx12@Naver.com>
2024-12-05 07:57:06 +0900

    Feat: 리액트쿼리 적용

--- src/app/(app)/my/reservation/components/ReservationList.tsx
+++ src/app/(app)/my/reservation/components/ReservationList.tsx
@@ -1,16 +1,27 @@
+"use client";
+
 import EmptyActivity from "@/components/EmptyActivity";
 import Dropdown from "@/components/dropdown/Dropdown";
+import { getMyReservation } from "@/lib/api/MyReservation";
+import { Reservation } from "@/types/MyReservationType";
+import { useQuery } from "@tanstack/react-query";
 import ReservationItem from "./ReservationItem";
-import { mockReservations } from "./mockData";
 
 export default function ReservationList() {
-  const hasReservations = mockReservations && mockReservations.reservations.length > 0;
+  const { data: reservations = [], isLoading } = useQuery<Reservation[]>({
+    queryKey: ["reservations"],
+    queryFn: getMyReservation,
+    retry: 1,
+  });
+  if (isLoading) return <div>Loading...</div>;
+
+  const hasReservations = reservations.length > 0;
 
   return (
     <section className="flex w-full max-w-[800px] flex-col bg-gray01">
       <header className="mb-5 flex justify-between">
         <h1 className="text-[32px] font-bold">예약내역</h1>
-        <span className="hidden xl:block">
+        <span className="hidden text-center text-3xl xl:block">
           <Dropdown
             label="필터"
             size="xl"
@@ -20,9 +31,7 @@ export default function ReservationList() {
       </header>
       <div className="flex flex-col gap-4">
         {hasReservations ? (
-          mockReservations.reservations.map((reservation) => (
-            <ReservationItem key={reservation.id} reservation={reservation} />
-          ))
+          reservations.map((reservation) => <ReservationItem key={reservation.id} reservation={reservation} />)
         ) : (
           <EmptyActivity />
         )}
