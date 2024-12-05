ac4e2d83290ecaa175536cf1b0ec02ab97d43200
ckx9798 <ckx12@Naver.com>
2024-12-05 17:55:43 +0900

    Feat: 예약내역 필터링 구현

--- src/app/(app)/my/reservation/components/ReservationList.tsx
+++ src/app/(app)/my/reservation/components/ReservationList.tsx
@@ -1,19 +1,48 @@
 "use client";
 
 import EmptyActivity from "@/components/EmptyActivity";
-import Dropdown from "@/components/dropdown/Dropdown";
 import { getMyReservation } from "@/lib/api/MyReservation";
 import { Reservation } from "@/types/MyReservationType";
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import ReservationDropdown from "./ReservationDropdown";
 import ReservationItem from "./ReservationItem";
 
 export default function ReservationList() {
+  const FILTER_OPTIONS = ["전체 보기", "예약 신청", "예약 취소", "예약 승인", "예약 거절", "체험 완료"];
+  const [filter, setFilter] = useState<string>(FILTER_OPTIONS[0]);
+
+  const filterReservations = (data: Reservation[], filter: string) => {
+    switch (filter) {
+      case FILTER_OPTIONS[0]:
+        return data;
+      case FILTER_OPTIONS[1]:
+        return data.filter((reservation) => reservation.status === "pending");
+      case FILTER_OPTIONS[2]:
+        return data.filter((reservation) => reservation.status === "canceled");
+      case FILTER_OPTIONS[3]:
+        return data.filter((reservation) => reservation.status === "confirmed");
+      case FILTER_OPTIONS[4]:
+        return data.filter((reservation) => reservation.status === "declined");
+      case FILTER_OPTIONS[5]:
+        return data.filter((reservation) => reservation.status === "completed");
+      default:
+        return data;
+    }
+  };
+
   const { data: reservations = [], isLoading } = useQuery<Reservation[]>({
     queryKey: ["reservations"],
     queryFn: getMyReservation,
+    select: (data) => filterReservations(data, filter),
     retry: 1,
   });
   if (isLoading) return <div>Loading...</div>;
+
+  const handleFilterChange = (value: string) => {
+    setFilter(value);
+    console.log(filter);
+  };
 
   const hasReservations = reservations.length > 0;
 
@@ -22,10 +51,12 @@ export default function ReservationList() {
       <header className="mb-5 flex justify-between">
         <h1 className="text-[32px] font-bold">예약내역</h1>
         <span className="hidden text-center text-3xl xl:block">
-          <Dropdown
+          <ReservationDropdown
             label="필터"
             size="xl"
-            options={["전체 보기", "예약 신청", "예약 취소", "예약 승인", "예약 거절", "체험 완료"]}
+            options={FILTER_OPTIONS}
+            value={filter}
+            onChange={(value: string) => handleFilterChange(value)}
           />
         </span>
       </header>
