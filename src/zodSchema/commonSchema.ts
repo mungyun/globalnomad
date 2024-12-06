a9bb43463825894a40d8470d16b81f602f636fa9
ckx9798 <ckx12@Naver.com>
2024-12-07 08:21:32 +0900

    Chore: 빌드 오류 해결

--- src/zodSchema/commonSchema.ts
+++ src/zodSchema/commonSchema.ts
@@ -4,12 +4,14 @@ import { z } from "zod";
 export const EmailSchema = z.string().email("이메일 형식으로 작성해 주세요.");
 
 // 비밀번호 정규 표현식 (regex)
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
 // 비밀번호 스키마
 export const PasswordSchema = z
   .string()
   .min(8, "비밀번호는 8자 이상이어야 합니다.")
-  .max(32, "비밀번호는 최대 32자까지 가능합니다.");
+  .max(32, "비밀번호는 최대 32자까지 가능합니다.")
+  .regex(passwordRegex, "영문 대소문자, 숫자, 특수문자를 모두 포함해야 합니다.");
 
 // 닉네임 정규 표현식 (regex)
 const nicknameRegex = /^[a-zA-Z0-9가-힣]+$/;
