8ccf60800d13fcbcb144abdaba12542819541d05
mungyun <mungyun1234@naver.com>
2024-12-06 23:07:05 +0900

    Feat: getReservationDashboard 함수 추가

--- src/lib/api/MyActivities.ts
+++ src/lib/api/MyActivities.ts
@@ -1,5 +1,6 @@
 // 내 체험 삭제
-import { proxy } from "./axiosInstanceApi";
+import { cookies } from "next/headers";
+import axiosInstance, { proxy } from "./axiosInstanceApi";
 
 export const deleteMyReservation = async (activityId: number) => {
   try {
@@ -7,5 +8,30 @@ export const deleteMyReservation = async (activityId: number) => {
     return response.data;
   } catch (error) {
     console.error("내 체험 삭제 오류: ", error);
+  }
+};
+
+// 내 체험 월별 예약 현황 조회
+
+export const getReservationDashboard = async ({
+  activityId,
+  year,
+  month,
+}: {
+  activityId: number;
+  year: string;
+  month: string;
+}) => {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+  const headers = accessToken ? { Authorization: `Bearer ${accessToken}` } : {};
+  try {
+    const response = await axiosInstance.get(
+      `/my-activities/${activityId}/reservation-dashboard?year=${year}&month=${month}`,
+      { headers }
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
   }
 };
