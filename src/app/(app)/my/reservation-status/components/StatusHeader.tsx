3019f585793e95fcbaa1e4ae6ad21f138ef53cff
mungyun <mungyun1234@naver.com>
2024-12-06 22:05:34 +0900

    Chore: StatusHeader 콘솔 제거

--- src/app/(app)/my/reservation-status/components/StatusHeader.tsx
+++ src/app/(app)/my/reservation-status/components/StatusHeader.tsx
@@ -2,10 +2,9 @@ import axiosInstance from "@/lib/api/axiosInstanceApi";
 import { cookies } from "next/headers";
 import StatusDropdown from "./StatusDropdown";
 
-const StatusHeader = async ({ cursorId = null, size = 20 }: { cursorId?: number | null; size?: number }) => {
+const StatusHeader = async ({ cursorId = null, size = 10 }: { cursorId?: number | null; size?: number }) => {
   const cookieStore = await cookies();
   const accessToken = cookieStore.get("accessToken")?.value;
-  console.log("Access Token:", accessToken);
 
   // Authorization 헤더 설정
   const headers = accessToken ? { Authorization: `Bearer ${accessToken}` } : {};
@@ -16,12 +15,14 @@ const StatusHeader = async ({ cursorId = null, size = 20 }: { cursorId?: number 
 
   // API 요청 보내기
   const response = await axiosInstance.get(url, { headers });
+  console.log(response.data);
 
   // 응답 데이터 처리
   const activityTitles = response.data.reservations.map(
     (reservation: { activity: { title: string } }) => reservation.activity.title
   );
 
+  console.log("activityTitles:", activityTitles);
   return (
     <div className="mb-[30px]">
       <h2 className="mb-6 text-[32px] font-bold md:mb-8">예약현황</h2>
