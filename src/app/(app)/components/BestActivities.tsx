5c46e17094469bd1051aca464c278cc039f78873
DrowsyzzZ <daenamu222@naver.com>
2024-12-09 15:49:53 +0900

    Feat: 스켈레톤 UI 추가

--- src/app/(app)/components/BestActivities.tsx
+++ src/app/(app)/components/BestActivities.tsx
@@ -3,6 +3,7 @@
 import { useCarousel } from "@/hooks/useCarousel";
 import useDeviceType from "@/hooks/useDeviceType";
 import { getActivities } from "@/lib/api/Activities";
+import BestActivitiesSkeleton from "@/skeleton/main/BestActivitiesSkeleton";
 import { GetActivities } from "@/types/ActivityType";
 import { useQuery } from "@tanstack/react-query";
 import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
@@ -24,20 +25,20 @@ const BestActivities = () => {
   const deviceType = useDeviceType();
 
   if (isLoading) {
-    return <div className="h-60 w-full md:h-[550px]">로딩중...</div>;
+    return <BestActivitiesSkeleton />;
   }
   if (error) {
-    return <div className="h-60 w-full md:h-[550px]">Error: {error.message}</div>;
+    return <div className="h-[223px] w-full md:h-[460px]">Error: {error.message}</div>;
   }
   if (!bestData) {
-    return <div className="h-60 w-full md:h-[550px]">배너 데이터가 없습니다.</div>;
+    return <div className="h-[223px] w-full md:h-[460px]">배너 데이터가 없습니다.</div>;
   }
 
   const { activities } = bestData;
 
   return (
-    <section className="flex flex-col gap-4 pl-4 md:gap-8 md:pl-6 xl:pl-0">
-      <div className="flex justify-between">
+    <section className="flex flex-col gap-4 md:gap-8">
+      <div className="flex justify-between pl-4 md:pl-6 xl:pl-0">
         <h2 className="text-lg font-bold leading-[21.48px] text-black03 md:text-4xl md:leading-[42.96px]">
           🔥 인기 체험
         </h2>
@@ -66,11 +67,13 @@ const BestActivities = () => {
       </div>
       <div
         ref={carouselRef}
-        className="flex w-full gap-4 overflow-x-scroll md:gap-8 xl:gap-6 xl:transition-transform xl:duration-500 xl:ease-in-out"
+        className="flex w-full gap-4 overflow-x-scroll md:gap-6 xl:transition-transform xl:duration-500 xl:ease-in-out"
       >
+        {deviceType === "desktop" ? "" : <div></div>}
         {activities.map((activity) => (
           <BestActivityItem key={activity.id} activity={activity} />
         ))}
+        {deviceType === "desktop" ? "" : <div></div>}
       </div>
     </section>
   );
