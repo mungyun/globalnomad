7dcee4b18ea17d134e1e81218ddf651f9a5c9992
ckx9798 <ckx12@Naver.com>
2024-12-12 08:41:05 +0900

    Feat: 내체험 삭제 로직 구현

--- src/app/api/my-activities/route.ts
+++ src/app/api/my-activities/route.ts
@@ -29,3 +29,25 @@ export const GET = async (req: NextRequest) => {
     return NextResponse.json({ message: "서버 오류" }, { status: 500 });
   }
 };
+
+export const DELETE = async (req: NextRequest, { params }: { params: Promise<{ activityId: string }> }) => {
+  const accessToken = req.cookies.get("accessToken")?.value;
+  const { activityId } = await params;
+
+  try {
+    const response = await axiosInstance.delete(`/my-activities/${activityId}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    return NextResponse.json(response.data, { status: 200 });
+  } catch (error: unknown) {
+    if (isAxiosError(error)) {
+      const errorMessage = error.response?.data?.message;
+      return NextResponse.json({ message: errorMessage }, { status: error.response?.status });
+    }
+
+    console.error(error);
+    return NextResponse.json({ message: "서버 오류" }, { status: 500 });
+  }
+};
