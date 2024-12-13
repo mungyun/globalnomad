8123e772586e1ceb6f41ddd0677ff3a0a788507b
KimChungO <kco796@naver.com>
2024-12-13 11:17:43 +0900

    Chore: CI/CD 오류 수정

--- src/app/(app)/my/activity/[activityId]/page.tsx
+++ src/app/(app)/my/activity/[activityId]/page.tsx
@@ -1,13 +1,11 @@
 import UpdateActivityForm from "./components/UpdateActivityForm";
 
 interface PageProps {
-  params: {
-    activityId: number;
-  };
+  params: Promise<{ activityId: number }>;
 }
 
-const UpdateActivityPage = ({ params }: PageProps) => {
-  const { activityId } = params;
+const UpdateActivityPage = async ({ params }: PageProps) => {
+  const { activityId } = await params;
   return (
     <div className="w-full max-w-[800px]">
       <UpdateActivityForm id={activityId} />
