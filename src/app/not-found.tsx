ca5b842546df1c2e7e617f39852888d69366dccd
BugO <kco796@naver.com>
2024-12-16 15:06:22 +0900

    Feat: 404 페이지 추가

---
+++ src/app/not-found.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const NotFound = () => {
+  return (
+    <div className="flex h-dvh flex-col items-center justify-center gap-6">
+      <Image src="/icons/logo_big.svg" className="h-[154px] w-[270px]" width={0} height={0} alt="logo" />
+      <div className="text-center">
+        <h2 className="text-lg font-bold">페이지를 찾을 수 없습니다.</h2>
+        <p>경로를 다시 확인해주세요!</p>
+      </div>
+      <Link
+        className="flex h-[48px] w-[350px] items-center justify-center rounded-md bg-green02 text-base font-bold leading-[26px] text-white"
+        href="/"
+      >
+        메인화면으로
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
