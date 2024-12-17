72f4c024171d2450a3714e457b3d8453ce0a4749
mungyun <mungyun1234@naver.com>
2024-12-17 15:43:40 +0900

    Fix: PatchActivities함수 주석 수정

--- src/lib/api/MyActivities.ts
+++ src/lib/api/MyActivities.ts
@@ -2,6 +2,22 @@ import { PatchActivityType } from "@/types/ActivityType";
 import { isAxiosError } from "axios";
 import { proxy } from "./axiosInstanceApi";
 
+// 내 체험 목록 조회
+export const getMyActivities = async ({ cursorId = null, size = 10 }: { cursorId?: number | null; size?: number }) => {
+  try {
+    let url = `/api/my-activities?size=${size}`;
+
+    if (cursorId) url += `&cursorId=${cursorId}`;
+    const response = await proxy.get(url);
+    return response.data.activities;
+  } catch (error: unknown) {
+    if (isAxiosError(error)) {
+      throw error;
+    }
+    throw error;
+  }
+};
+
 // 내 체험 삭제
 export const deleteMyActivities = async (activityId: number) => {
   try {
@@ -15,14 +31,11 @@ export const deleteMyActivities = async (activityId: number) => {
   }
 };
 
-// 내 체험 목록 조회
-export const getMyActivities = async ({ cursorId = null, size = 10 }: { cursorId?: number | null; size?: number }) => {
+// 내 체험 수정
+export const PatchActivities = async (activityId: number, data: PatchActivityType) => {
   try {
-    let url = `/api/my-activities?size=${size}`;
-
-    if (cursorId) url += `&cursorId=${cursorId}`;
-    const response = await proxy.get(url);
-    return response.data.activities;
+    const res = await proxy.patch(`/api/my-activities/${activityId}`, data);
+    return res.data;
   } catch (error: unknown) {
     if (isAxiosError(error)) {
       throw error;
@@ -110,19 +123,6 @@ export const getMyActivitiesByMonth = async ({
       `/api/my-activities/${activityId}/reservation-dashboard?year=${year}&month=${month}`
     );
     return response.data;
-  } catch (error: unknown) {
-    if (isAxiosError(error)) {
-      throw error;
-    }
-    throw error;
-  }
-};
-
-// 내 체험 예약 상태(승인, 거절) 업데이트
-export const PatchActivities = async (activityId: number, data: PatchActivityType) => {
-  try {
-    const res = await proxy.patch(`/api/my-activities/${activityId}`, data);
-    return res.data;
   } catch (error: unknown) {
     if (isAxiosError(error)) {
       throw error;
