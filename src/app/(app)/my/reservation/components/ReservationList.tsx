2245014a6141260d458dddd7ffbff322d4ab246a
ckx9798 <ckx12@Naver.com>
2024-12-20 13:09:01 +0900

    Feat: 필터 적용 및 min-h 적용

--- src/app/(app)/my/reservation/components/ReservationList.tsx
+++ src/app/(app)/my/reservation/components/ReservationList.tsx
@@ -12,15 +12,15 @@ import { SyncLoader } from "react-spinners";
 import ReservationDropdown from "./ReservationDropdown";
 import ReservationItem from "./ReservationItem";
 
-type ReservationStatus = "all" | "pending" | "canceled" | "confirmed" | "declined" | "completed";
+// type ReservationStatus = null | "pending" | "canceled" | "confirmed" | "declined" | "completed";
 
 interface FilterOption {
-  value: ReservationStatus;
+  value: string;
   label: string;
 }
 
 const FILTER_OPTIONS: FilterOption[] = [
-  { value: "all", label: "전체 보기" },
+  { value: "", label: "전체 보기" },
   { value: "pending", label: "예약 신청" },
   { value: "canceled", label: "예약 취소" },
   { value: "confirmed", label: "예약 승인" },
@@ -29,7 +29,7 @@ const FILTER_OPTIONS: FilterOption[] = [
 ];
 
 export default function ReservationList() {
-  const [filter, setFilter] = useState<ReservationStatus>("all");
+  const [filter, setFilter] = useState("");
 
   const { ref, inView } = useInView();
 
@@ -40,10 +40,7 @@ export default function ReservationList() {
   //   retry: 1,
   // });
 
-  const { data, fetchNextPage, hasNextPage, isFetching, isLoading } = useInfinityItems({
-    status: "canceld",
-    cursorId: null,
-  });
+  const { data, fetchNextPage, hasNextPage, isFetching, isLoading } = useInfinityItems("reservation", filter);
   // const { data, fetchNextPage, hasNextPage, isFetching, isLoading } = useInfinityItems();
 
   // const { data, isLoading, fetchNextPage, hasNextPage, isFetching } = useInfiniteQuery({
@@ -60,7 +57,7 @@ export default function ReservationList() {
   //   },
   //   initialPageParam: undefined,
   // });
-  console.log(data);
+  console.log(data?.pages);
 
   useEffect(() => {
     if (inView && hasNextPage && !isFetching) {
@@ -70,13 +67,14 @@ export default function ReservationList() {
 
   const handleFilterChange = (selectedLabel: string) => {
     const selectedOption = FILTER_OPTIONS.find((option) => option.label === selectedLabel);
+    console.log(selectedLabel);
     if (selectedOption) setFilter(selectedOption.value);
   };
 
   if (isLoading) return <ReservationSkeleton />;
 
   return (
-    <section className="flex w-full max-w-[800px] flex-col bg-gray01">
+    <section className="flex min-h-screen w-full max-w-[800px] flex-col bg-gray01">
       <header className="mb-5 flex justify-between">
         <h1 className="text-[32px] font-bold">예약내역</h1>
         <span className="hidden text-center text-3xl xl:block">
@@ -91,11 +89,13 @@ export default function ReservationList() {
       </header>
 
       <div className="flex flex-col gap-4">
-        {data?.pages.map((page) => {
-          return page.reservations.map((reservation) => {
-            return <ReservationItem reservation={reservation} />;
-          });
-        })}
+        {data?.pages.map((page) =>
+          page.totalCount === 0 ? (
+            <EmptyActivity />
+          ) : (
+            page.reservations.map((reservation) => <ReservationItem key={reservation.id} reservation={reservation} />)
+          )
+        )}
         {/* {reservations.length > 0 ? (
           reservations.map((reservation) => <ReservationItem key={reservation.id} reservation={reservation} />)
         ) : (
