e208bbf0097f22c82ec6a8a904e2218fa31c870c
mungyun <mungyun1234@naver.com>
2024-12-23 15:53:02 +0900

    Refactor: 모바일에서 카카오 지도 높이 수정

--- src/app/(app)/activities/[activityId]/components/KakaoMap.tsx
+++ src/app/(app)/activities/[activityId]/components/KakaoMap.tsx
@@ -28,7 +28,7 @@ const KakaoMap = ({ address }: KakaoMapProps) => {
 
   return (
     <>
-      <div className="relative z-0 h-[482px] w-full md:h-[276px] xl:h-[450px]">
+      <div className="relative z-0 h-[343px] w-full md:h-[276px] xl:h-[450px]">
         <Map center={coordinates} style={{ width: "100%", height: "100%" }} level={3}>
           <MapMarker position={coordinates}></MapMarker>
         </Map>
