80a46907b97c86b2745dc6fe1803fd4fb4c0fc82
mungyun <mungyun1234@naver.com>
2024-12-24 14:40:04 +0900

    Chore: 체험 상세 페이지 이미지 확대 시, 뒷 배경 스크롤 차단

--- src/app/(app)/activities/[activityId]/components/ImageModal.tsx
+++ src/app/(app)/activities/[activityId]/components/ImageModal.tsx
@@ -1,12 +1,24 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
 
 const ImageModal = ({ selectedImage, onClose }: { selectedImage: string; onClose: () => void }) => {
+  useEffect(() => {
+    // 모달이 열릴 때 스크롤 방지
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      // 모달이 닫힐 때 스크롤 복원
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+
   return (
     <div>
       <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-70">
         <div className="relative">
+          {/* 확대된 이미지 */}
           <Image src={selectedImage} alt="확대된 이미지" width={800} height={600} className="rounded-xl object-cover" />
+          {/* 닫기 버튼 */}
           <button onClick={onClose} className="absolute right-2 top-2 rounded-full bg-white">
             <Image src="/icons/X.svg" width={30} height={30} alt="닫기" />
           </button>
