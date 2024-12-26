9a369757c63f8d0a09db32cea26a14383e918f63
BugO <kco796@naver.com>
2024-12-26 13:21:42 +0900

    Chore: 회원가입 페이지 소셜 로그인 적용

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,20 +1,18 @@
 import SignupForm from "@/app/(auth)/components/SignupForm";
-import SocialSignup from "@/app/(auth)/components/SocialSignup";
 import Link from "next/link";
+import SocialLogin from "../components/SocialLogin";
 
 const SignupPage = () => {
   return (
     <>
       <SignupForm />
-
       <p className="text-base leading-[19.09px] text-gray09">
         회원이신가요?{" "}
         <Link href="/login" className="text-green02 underline">
           로그인하기
         </Link>
       </p>
-
-      <SocialSignup />
+      <SocialLogin />
     </>
   );
 };
